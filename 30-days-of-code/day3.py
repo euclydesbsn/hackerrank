53d47935cf75783086cee1ed3892e8e343d5bdb4
Euclydes Brasil <euclydesbsn@gmail.com>
2016-04-11 23:56:30 -0300

    Que tal usar elif ao invés de if dentro de else?

--- 30-days-of-code/day3.py
+++ 30-days-of-code/day3.py
@@ -2,12 +2,9 @@ n = int(raw_input())
 
 if (n % 2 == 1):
 	print "Weird"
-else:
-	if (n >= 2 and n <= 5):
-		print "Not Weird"
-	else:
-		if (n >= 6 and n <= 20):
-			print "Weird"
-		else:
-			if (n > 20):
-				print "Not Weird"
+elif (n >= 2 and n <= 5):
+	print "Not Weird"
+elif (n >= 6 and n <= 20):
+	print "Weird"
+elif (n > 20):
+	print "Not Weird"
