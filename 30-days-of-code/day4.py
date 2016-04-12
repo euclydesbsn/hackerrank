5714961fdc4b1bd3bf9a9aea554015dca4c88573
Euclydes Brasil <euclydesbsn@gmail.com>
2016-04-12 00:08:32 -0300

    Usando self ao invés de global

--- 30-days-of-code/day4.py
+++ 30-days-of-code/day4.py
@@ -1,17 +1,17 @@
 class Person:
     def __init__(self,initialAge):
         if initialAge < 0:
-        	print "Age is not valid, setting age to 0."
-        	age = 0
+            print "Age is not valid, setting age to 0."
+            self.age = 0
         else:
-        	age = initialAge
+            self.age = initialAge
     def amIOld(self):
-        if age < 13:
-        	print "You are young."
-        elif age >= 13 and age < 18:
-        	print "You are a teenager."
+        if self.age < 13:
+            print "You are young."
+        elif self.age >= 13 and self.age < 18:
+            print "You are a teenager."
         else:
-        	print "You are old."
+            print "You are old."
     def yearPasses(self):
     	global age
-        age += 1
+        self.age += 1
