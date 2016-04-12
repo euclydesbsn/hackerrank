e5847975b7e67f4a42236c39e22029dc6fbbf310
Euclydes Brasil <euclydesbsn@gmail.com>
2016-04-11 23:52:38 -0300

    totalCost recebe valor exato e arredondamento feito no print

--- 30-days-of-code/day2.py
+++ 30-days-of-code/day2.py
@@ -2,6 +2,6 @@ mealCost = float(raw_input())
 tipPercent = float(raw_input())
 taxPercent = float(raw_input())
 
-totalCost = round(mealCost + (mealCost*(tipPercent/100)) + (mealCost*(taxPercent/100)))
+totalCost = mealCost + (mealCost*(tipPercent/100)) + (mealCost*(taxPercent/100))
 
-print "The total meal cost is " + str(int(totalCost)) + " dollars."
+print "The total meal cost is " + str(int(round(totalCost))) + " dollars."
