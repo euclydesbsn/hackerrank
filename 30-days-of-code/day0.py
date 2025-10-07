d008de1ec0669657d5fc933808484c954b3aa6fc
euclydesbsn <euclydesbsn@gmail.com>
2025-10-07 18:40:09 +0100

    I'm starting the 30 days of code challenge all over again. I'm moving the old solutions to a subdirectory and will be creating new files for the new solutions.

--- 30-days-of-code/day0.py
+++ 30-days-of-code/day0.py
@@ -1,4 +1,8 @@
-inputString = raw_input()
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+input_string = input()
 
-print 'Hello, World.'
-print inputString
+# Print a string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+
+# TODO: Write a line of code here that prints the contents of input_string to stdout.
+print(input_string)
